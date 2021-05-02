2e8c4792ec7d0d3aff0483a7a930454247a00571
lkhyeon <lkdh97@gmail.com>
2021-05-01 20:41:58 -0400

    Temporary styles set for header and the search bar

---
+++ src/components/search.js
@@ -0,0 +1,12 @@
+import "./css/search.css"
+
+const Search = () => {
+	return (
+		<div className="Search">
+			<p> MOVIE TITLE </p>
+			<input placeholder={"Search a movie"} />
+		</div>
+	)
+}
+
+export default Search;
